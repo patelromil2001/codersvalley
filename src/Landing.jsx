f5d9dacd23d41f595146c6fdfa16b82d806c70aa
Romil <patelromil.2001@gmail.com>
2023-03-10 11:21:11 +0530

    10th march

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,12 +1,17 @@
 const Landing = () => {
+
+    
     return (
-        <div class="main bg-cyan-50 text-5xl pl-40 pt-16 font-bold space-y-2 bg-gradient-to-r from-slate-100 to-indigo-500" >
+        <div>
+            <div class="main bg-cyan-50 text-5xl pl-40 py-32 font-bold space-y-2 object-right-bottom bg-[url('https://thumb.tildacdn.com/tild3834-3666-4836-a136-613033396335/-/format/webp/Group_32.png')] from-slate-100 to-indigo-500" >
             <p><span class="text-blue-800">Help</span> US-based companies</p> 
             <p><span class="text-blue-800">hire</span> incredible English-speaking</p> 
             <p><span class="text-blue-800">remote</span> software engineers</p> 
             <p>from Latin America</p> 
-
+            </div>
         </div>
+        
+        
     );
 }
 
