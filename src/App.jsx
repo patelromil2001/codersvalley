fa2117c0240473d96598823adf22b706db6c0fcd
Romil <patelromil.2001@gmail.com>
2023-03-13 14:52:30 +0530

    About Page End

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@ import Navbar from './Navbar';
 import Home from './Home';
 import Footer from './Footer';
 import About from './About';
-
+import Salaries from './Salaries'
 
 function App() {
   return (
-    <div className="overflow-hidden">
+    <div>
     <Navbar />
     <Home />
     <About/>
-    
+    <Salaries/>
     <Footer/>
     </div>
     
