1cf140c5f3af6985bfb5de00ca9cd7b725f7835e
Romil <patelromil.2001@gmail.com>
2023-03-16 16:01:23 +0530

    123

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,27 +1,47 @@
+
 const Home = () => {
 
     return (
-       <div className="home">
-            <div class="main bg-cyan-50 text-5xl pl-40 pt-32 font-bold space-y-2 object-right-bottom bg-[url('https://thumb.tildacdn.com/tild3834-3666-4836-a136-613033396335/-/format/webp/Group_32.png')] from-slate-100 to-indigo-500" >
-            <p><span class="text-blue-800">Help</span> US-based companies</p> 
-            <p><span class="text-blue-800">hire</span> incredible English-speaking</p> 
-            <p><span class="text-blue-800">remote</span> software engineers</p> 
-            <p>from Latin America</p> 
-            <div className="py-10">
-                <div className="rectangle inline-block bg-cyan-200 w-[36rem] h-14 rounded ">
-                    <img src="https://cdn-icons-png.flaticon.com/512/3031/3031293.png" className= "pt-4 h-10 pl-6" alt="1" />
+        <div className="home">
+
+            <div class="main w-full  sm:inline-block  lg:flex bg-cyan-50 text-5xl pl-10 md:pl-20 lg:pl-40 pt-32 font-bold space-y-2 object-right-bottom bg-[url('https://thumb.tildacdn.com/tild3834-3666-4836-a136-613033396335/-/format/webp/Group_32.png')] from-slate-100 to-indigo-500" >
+                <div>
+                    <div>
+                        <p><span class="text-blue-800">Help</span> US-based companies</p>
+                        <p><span class="text-blue-800">hire</span> incredible English-speaking</p>
+                        <p><span class="text-blue-800">remote</span> software engineers</p>
+                        <p>from Latin America</p>
+                    </div>
+
+                    <div className="py-10">
+                        <div className="rectangle inline-block bg-cyan-200 w-[75%]  lg:w-[36rem] h-14 rounded ">
+                            <img src="https://cdn-icons-png.flaticon.com/512/3031/3031293.png" className="pt-4 h-10 pl-6" alt="1" />
+                        </div>
+                    </div>
+
+                    <div>
+                        <p className="text-[28px]">Save from $29,000 to $74,000 a year per talent*</p>
+                        <p className="text-[9px]">*based on salary data from the indeed.com and talent.com</p>
+                        <p className="text-[15px] pt-9"><span className="font-bold">Countries we've worked with:</span> Brazil, Argentina, Chile, Uruguay, Costa Rica, Ecuador, Columbia,</p>
+                        <p className="text-[15px] pb-9"> Peru, Mexico, Cuba, Dominican Republic, Panama</p>
+                    </div>
+                </div>
+                <div className="px-16 py-14 lg:pl-36">
+                    <div className="pb-3"><div className="rectangle w-96 h-16 rounded-xl bg-gradient-to-r from-red-400 to-yellow-400"> <p className="text-[11px] pt-1 pl-3">Our Sucess Fee</p> <p className="text-2xl font-bold pl-3">$4.800</p> </div></div>
+                    <div className="flex">
+                        <div className=""><div className="rectangle w-28 h-10 rounded-xl bg-gradient-to-r from-red-400 to-yellow-400"> <p className="text-[11px] pt-1 pl-3">Our Sucess Fee</p> <p className="text-2xl font-bold pl-3">$4.800</p> </div></div>
+                        <div className="pl-3"><div className="rectangle w-10 h-36 rounded-xl bg-gradient-to-r from-red-400 to-yellow-400"> <p className="text-[11px] pt-1 pl-3">Our Sucess Fee</p> <p className="text-2xl font-bold pl-3">$4.800</p> </div></div>
+                        <div className="pl-3"><div className="rectangle w-52 h-10 rounded-xl bg-gradient-to-r from-red-400 to-yellow-400"> <p className="text-[11px] pt-1 pl-3">Our Sucess Fee</p> <p className="text-2xl font-bold pl-3">$4.800</p> </div></div>
+                    </div>
                 </div>
             </div>
-            <p className="text-[28px]">Save from $29,000 to $74,000 a year per talent*</p>
-            <p className="text-[9px]">*based on salary data from the indeed.com and talent.com</p>
-            <p className="text-[15px] pt-9"><span className="font-bold">Countries we've worked with:</span> Brazil, Argentina, Chile, Uruguay, Costa Rica, Ecuador, Columbia,</p>
-            <p className="text-[15px] pb-9"> Peru, Mexico, Cuba, Dominican Republic, Panama</p>
-            </div>
-            <div className="bg-cyan-100">
-                <div className="ml-16 bg-white pl-24  h-[400px]">
+            <div className=" bg-white flex">
+                <div className=" h-auto w-[10%] md:w-[8%] lg:[6%] bg-cyan-50"></div>
+
+                <div className=" pl-24">
                     <p className="text-4xl pt-9">Companies we've worked with</p>
                     <p className="text-xl pt-4">150+ US and UK-based clients</p>
-                    <div className="grid pt-10 pr-36 gap-14 grid-cols-5 grid-rows-3">
+                    <div className="py-5 space-y-10 md:space-y-0 lg:space-y-0 grid md:grid-cols-3 md:grid-rows-3 md:gap-10 lg:pt-10 lg:pr-36 lg:gap-20 lg:grid-cols-5 lg:grid-rows-3">
                         <a href="https://digitalgenius.com/"><img src="https://digitalgenius.com/wp-content/uploads/2021/06/logo-dark.svg" alt="" /></a>
                         <a href="https://digitalgenius.com/"><img src="https://digitalgenius.com/wp-content/uploads/2021/06/logo-dark.svg" alt="" /></a>
                         <a href="https://digitalgenius.com/"><img src="https://digitalgenius.com/wp-content/uploads/2021/06/logo-dark.svg" alt="" /></a>
@@ -41,11 +61,11 @@ const Home = () => {
                 </div>
             </div>
         </div>
-   
-    
+
+
     )
 }
-        
-        
+
+
 
 export default Home;
--- src/Process.jsx
+++ src/Process.jsx
@@ -13,7 +13,7 @@ const Process = () => {
                     <p className="text-base pl-7 pt-4">We edit your vacancy for the specifics of the Latin <br /> America market and start the search.</p>
                 </div>
                 <div className="Image pl-24">
-                    <img src="https://thumb.tildacdn.com/tild6265-3636-4830-a562-336630393935/-/format/webp/image_10.png" alt="" />
+                    {/* <img src="https://thumb.tildacdn.com/tild6265-3636-4830-a562-336630393935/-/format/webp/image_10.png" alt="" /> */}
                 </div>
             </div>
             <div className="Step-2 flex pl-44">
@@ -26,7 +26,7 @@ const Process = () => {
                     <p className="text-base pl-7 pt-4">You will receive your first 2-3 candidates by email within <br /> 2 working days.</p>
                 </div>
                 <div className="Image">
-                    <img src="https://static.tildacdn.com/tild6464-3138-4662-a233-643139393133/Group_33.svg" alt="" />
+                    {/* <img src="https://static.tildacdn.com/tild6464-3138-4662-a233-643139393133/Group_33.svg" alt="" /> */}
                     {/* images 
                     0 https://static.tildacdn.com/tild6133-6464-4335-a434-666261356562/Group_47.svg
                     1 https://static.tildacdn.com/tild3433-6364-4634-b937-383537353465/Group_34.svg
@@ -49,8 +49,8 @@ const Process = () => {
                     <p className="text-base pl-7 pt-4">You will receive another 7-10 candidates within 1-2 weeks <br /> (it depends on the complexity of the job). We will <br /> organize a joint interview of your team with the <br /> candidates that you like.</p>
                 </div>
                 <div className="Image flex">
-                    <img src="https://thumb.tildacdn.com/tild3231-3261-4637-b138-376437666161/-/format/webp/_1_1_1.png" alt="" />
-                    <div className=""><img src="https://static.tildacdn.com/tild6536-6462-4638-a633-343362346137/320_px_10_fps.gif" className="relative" alt="" /></div>
+                    {/* <img src="https://thumb.tildacdn.com/tild3231-3261-4637-b138-376437666161/-/format/webp/_1_1_1.png" alt="" /> */}
+                    {/* <div className=""><img src="https://static.tildacdn.com/tild6536-6462-4638-a633-343362346137/320_px_10_fps.gif" className="relative" alt="" /></div> */}
                     
                 </div>
             </div>
@@ -66,7 +66,7 @@ const Process = () => {
                     <p className="text-base pl-7 pt-4">Once you've hired your developer, our relationship is <br /> complete and a fee is paid.</p>
                 </div>
                 <div className="Image">
-                    <img src="https://static.tildacdn.com/tild3935-3036-4563-b765-353938626361/Group_48.svg" alt="" />
+                    {/* <img src="https://static.tildacdn.com/tild3935-3036-4563-b765-353938626361/Group_48.svg" alt="" /> */}
                 </div>
             </div>
             
