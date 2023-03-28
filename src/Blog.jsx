fabc0b9038c95fc7652cf39768bb63111d4e0151
Dhruv Parmar <dhruv.p8601@gmail.com>
2023-03-28 11:50:05 +0530

    blog changes

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -34,13 +34,19 @@ const Blog = () => {
                         blogs.map((blog) =>
                             <div className="p-3 rounded-xl group sm:flex space-x-6 bg-white bg-opacity-50 shadow-lg hover:shadow-2xl transition duration-500">
                                 <div className="pl-0 p-5">
-                                    <div className="space-y-2">
+                                    <div className="space-y-2 h-full flex flex-col">
                                         <div className="space-y-4">
                                             <h4 className="text-2xl ml-2 font-semibold text-cyan-900">{blog.title}</h4>
                                             <p className="text-gray-600 ml-2">{blog.description}</p>
                                         </div>
+                                        <div className="flex-grow"></div>
                                         <div>
-                                        <a href="#" className="block w-max text-cyan-600 ml-2">Read more</a></div>
+                                            <a href="#" className=" w-max flex text-cyan-600 ml-2">
+                                                <span>Read more</span>
+                                            <svg  xmlns="http://www.w3.org/2000/svg" fill="blue" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#0891b2" className=" mt-[1.5px] ml-1 w-6 h-6">
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
+                                            </svg></a>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
