bc42f5a5f2ca0e619578f4be7ed6790aa07dc343
Romil <patelromil.2001@gmail.com>
2023-03-28 11:48:27 +0530

    7654

--- src/Salaries.jsx
+++ src/Salaries.jsx
@@ -100,7 +100,7 @@ function Salaries() {
       <div className="bg-gray-200">
         <section className="text-gray-600 body-font">
           <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-            <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+            <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0 lg:pl-20">
               <img className="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600" />
             </div>
             <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
